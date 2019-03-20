88be5e40abc030b134eb8bddb345fb48941f4a9a
josephruocco <Joseph.Ruocco21@myhunter.cuny.edu>
2019-03-19 23:16:44 -0400

    added emacs config

---
+++ .emacs.d/elpa/yasnippet-snippets-20190309.1839/yasnippet-snippets-autoloads.el
@@ -0,0 +1,29 @@
+;;; yasnippet-snippets-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "yasnippet-snippets" "yasnippet-snippets.el"
+;;;;;;  (23691 47550 974950 347000))
+;;; Generated autoloads from yasnippet-snippets.el
+
+(autoload 'yasnippet-snippets-initialize "yasnippet-snippets" "\
+Load the `yasnippet-snippets' snippets directory.
+
+\(fn)" nil nil)
+
+(eval-after-load 'yasnippet '(yasnippet-snippets-initialize))
+
+;;;***
+
+;;;### (autoloads nil nil ("yasnippet-snippets-pkg.el") (23691 47625
+;;;;;;  771768 824000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; yasnippet-snippets-autoloads.el ends here
