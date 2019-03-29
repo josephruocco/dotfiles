afb20f759862dda7d866343500a41a5bf74bc923
josephruocco <Joseph.Ruocco21@myhunter.cuny.edu>
2019-03-29 11:39:50 -0400

    elpa

---
+++ emacs/.emacs.d/elpa/moe-theme-20180617.200/moe-theme-autoloads.el
@@ -0,0 +1,25 @@
+;;; moe-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "moe-theme" "moe-theme.el" (23441 21738 896196
+;;;;;;  907000))
+;;; Generated autoloads from moe-theme.el
+
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("moe-dark-theme.el" "moe-light-theme.el"
+;;;;;;  "moe-theme-pkg.el" "moe-theme-switcher.el") (23441 21739
+;;;;;;  147813 903000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; moe-theme-autoloads.el ends here
